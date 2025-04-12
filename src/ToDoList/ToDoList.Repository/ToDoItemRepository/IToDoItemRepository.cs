1d573b435f8a8c74a3658ef0734da78b28ea251d
Ahliddin <fahriddinovahliddin687@gmail.com>
2025-04-12 13:11:47 +0500

    ToDoList Repository done

--- src/ToDoList/ToDoList.Repository/ToDoItemRepository/IToDoItemRepository.cs
+++ src/ToDoList/ToDoList.Repository/ToDoItemRepository/IToDoItemRepository.cs
@@ -4,12 +4,12 @@ namespace ToDoList.Repository.ToDoItemRepository;
 
 public interface IToDoItemRepository
 {
-    Task InsertToDoItemAsync(ToDoItem toDoItem);
+    Task<long> InsertToDoItemAsync(ToDoItem toDoItem);
     Task DeleteToDoItemByIdAsync(long id);
     Task UpdateToDoItemAsync(ToDoItem toDoItem);
     Task<ICollection<ToDoItem>> SelectAllToDoItemsAsync(int skip, int take);
     Task<ToDoItem> SelectToDoItemByIdAsync(long id);
-    Task<ICollection<ToDoItem>> SelectByDueDateAsync(DateTime dueDate, int skip, int take);
+    Task<ICollection<ToDoItem>> SelectByDueDateAsync(DateTime dueDate);
     Task<ICollection<ToDoItem>> SelectCompletedAsync(int skip, int take);
     Task<ICollection<ToDoItem>> SelectIncompleteAsync(int skip, int take);
 }
