b83cf838d74ac6b5350e5ff66e3da823cef81211
m2222205 <abduhalilovamaftuna1@gmail.com>
2025-04-12 12:46:42 +0500

    done

--- src/ToDoList/ToDoList.Bll/Services/IToDoItemService.cs
+++ src/ToDoList/ToDoList.Bll/Services/IToDoItemService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ToDoList.Bll.DTOs;
+﻿using ToDoList.Bll.DTOs;
 using ToDoList.Dal.Entity;
 
 namespace ToDoList.Bll.Services
@@ -15,8 +10,8 @@ namespace ToDoList.Bll.Services
         List<ToDoItemGetDto> SelectAllToDoItemsAsync(int skip, int take);
         Task InsertToDoItemAsync(ToDoItem toDoItem);
         Task<long> DeleteToDoItemByIdAsync(long id);
-        Task<ToDoItemGetDto>  UpdateToDoItemAsync(ToDoItem oldItem, ToDoItem newItem);
-        Task <List<ToDoItemGetDto>> SelectCompletedAsync(int skip, int take);
+        Task<ToDoItemGetDto> UpdateToDoItemAsync(ToDoItem oldItem, ToDoItem newItem);
+        Task<List<ToDoItemGetDto>> SelectCompletedAsync(int skip, int take);
         Task<List<ToDoItemGetDto>> SelectIncompleteAsync(int skip, int take);
 
     }
