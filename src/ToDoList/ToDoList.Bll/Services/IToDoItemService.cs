ed94ab0e83df845955373545f6c7241e2adaef11
m2222205 <abduhalilovamaftuna1@gmail.com>
2025-04-12 13:11:51 +0500

    Changes <>

--- src/ToDoList/ToDoList.Bll/Services/IToDoItemService.cs
+++ src/ToDoList/ToDoList.Bll/Services/IToDoItemService.cs
@@ -1,13 +1,12 @@
 ﻿using ToDoList.Bll.DTOs;
-using ToDoList.Dal.Entity;
 
 namespace ToDoList.Bll.Services
 {
     public interface IToDoItemService
     {
         List<ToDoItemGetDto> SelectByDueDateAsync(DateTime dueDate);
-        ToDoItemGetDto SelectToDoItemByIdAsync(long id);
-        List<ToDoItemGetDto> SelectAllToDoItemsAsync(int skip, int take);
+        Task<ToDoItemGetDto> SelectToDoItemByIdAsync(long id);
+        Task<List<ToDoItemGetDto>> SelectAllToDoItemsAsync(int skip, int take);
         Task InsertToDoItemAsync(ToDoItemCreateDto toDoItem);
         Task<long> DeleteToDoItemByIdAsync(long id);
         Task<ToDoItemGetDto> UpdateToDoItemAsync(ToDoItemGetDto newItem);
