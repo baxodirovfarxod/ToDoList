c73401108aad65eac409d027daa5cf37cacb09d2
m2222205 <abduhalilovamaftuna1@gmail.com>
2025-04-12 13:14:53 +0500

    ezildim

--- src/ToDoList/ToDoList.Bll/Services/IToDoItemService.cs
+++ src/ToDoList/ToDoList.Bll/Services/IToDoItemService.cs
@@ -7,7 +7,7 @@ namespace ToDoList.Bll.Services
         Task<List<ToDoItemGetDto>> SelectByDueDateAsync(DateTime dueDate);
         Task<ToDoItemGetDto> SelectToDoItemByIdAsync(long id);
         Task<List<ToDoItemGetDto>> SelectAllToDoItemsAsync(int skip, int take);
-        Task InsertToDoItemAsync(ToDoItemCreateDto toDoItem);
+        Task<long> InsertToDoItemAsync(ToDoItemCreateDto toDoItem);
         Task<long> DeleteToDoItemByIdAsync(long id);
         Task<ToDoItemGetDto> UpdateToDoItemAsync(ToDoItemGetDto newItem);
         Task<List<ToDoItemGetDto>> SelectCompletedAsync(int skip, int take);
