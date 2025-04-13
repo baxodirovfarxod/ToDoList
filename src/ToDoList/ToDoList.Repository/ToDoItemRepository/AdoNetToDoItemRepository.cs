89a1df6504ed1bb840ea5ef512707a1c67692cec
baxodirovfarxod <baxodirovfarxod@2007.com>
2025-04-13 08:13:16 +0500

    Create ToDoItem repository using ADO.NET

---
+++ src/ToDoList/ToDoList.Repository/ToDoItemRepository/AdoNetToDoItemRepository.cs
@@ -0,0 +1,46 @@
+﻿using ToDoList.Dal.Entity;
+
+namespace ToDoList.Repository.ToDoItemRepository;
+
+public class AdoNetToDoItemRepository : IToDoItemRepository
+{
+    public Task DeleteToDoItemByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<long> InsertToDoItemAsync(ToDoItem toDoItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ICollection<ToDoItem>> SelectAllToDoItemsAsync(int skip, int take)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ICollection<ToDoItem>> SelectByDueDateAsync(DateTime dueDate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ICollection<ToDoItem>> SelectCompletedAsync(int skip, int take)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ICollection<ToDoItem>> SelectIncompleteAsync(int skip, int take)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ToDoItem> SelectToDoItemByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateToDoItemAsync(ToDoItem toDoItem)
+    {
+        throw new NotImplementedException();
+    }
+}
